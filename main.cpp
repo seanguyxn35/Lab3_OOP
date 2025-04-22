53a63c0c56c415139ca8723f486f865cebd8bae1
seanguyxn35 <seanguyen0305@gmail.com>
2025-04-22 16:41:07 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "cNhanVienVP.h"
+using namespace std;
+
+int main() {
+	cNhanVienVP DS;
+	DS.NhapDS();
+	DS.XuatDS();
+	DS.LuongCaoNhat();
+	DS.TuoiCaoNhat();
+	DS.TongLuong();
+	DS.sortDS();
+
+}
